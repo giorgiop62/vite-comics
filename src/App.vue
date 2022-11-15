712ac2062d4ae7fe27d6f7ce7ab23f1b5926e804
giorgip62 <giorgiop62@gmail.com>
2022-11-15 16:30:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/Appmain.vue';
+import AppFooter from './components/Appfooter.vue';
+
+  export default {
+    name:'App',
+    components:{
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
+  
+  }
+</script>
+
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+
+  
+</template>
+
+
+<style>
+
+</style>
